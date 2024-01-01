ba99c25ff255a8a315a3c30ba1b362f1f25164cf
myknuckles <evancopp86@gmail.com>
2024-01-01 10:32:46 -0500

    fixed game add issue

--- app/src/Controller/GamesController.php
+++ app/src/Controller/GamesController.php
@@ -17,11 +17,7 @@ class GamesController extends AppController
         $this->set(compact('game'));
     }
 
-<<<<<<< Updated upstream
-	public function add()
-=======
     public function add()
->>>>>>> Stashed changes
     {
         if ($this->request->is('post')) {
             $games = $this->Games->newEntity($this->request->getData());
@@ -29,14 +25,11 @@ class GamesController extends AppController
             if ($this->Games->save($games)) {
                 $this->Flash->success(__('The game has been saved.'));
                 return $this->redirect(['action' => 'index']);
-<<<<<<< Updated upstream
             } else {
                 $this->Flash->error(__('Unable to add the game.'));
             }
             $this->set('games', $games);
-=======
             }
->>>>>>> Stashed changes
-        }
     }
+
 }
--- app/templates/Games/entry.php
+++
@@ -1,8 +0,0 @@
-<h1>Add Game</h1>
-<?= $this->Form->create($games) ?>
-<?= $this->Form->create(null, ['url' => ['controller' => 'Games', 'action' => 'add']]) ?>
-    <?= $this->Form->control('title') ?>
-    <?= $this->Form->control('year')?>
-    <?= $this->Form->control('year') ?>
-    <?= $this->Form->button(__('Submit')) ?>
-<?= $this->Form->end() ?>
